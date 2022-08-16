9fccaa258b065bf53d1344ce359fb8eae608b07a
LauraAidee <vazquez.aidee@outlook.com>
2022-08-16 18:19:18 +0000

    Osito

---
+++ docker_fastapi/app/main.py
@@ -0,0 +1,30 @@
+from typing import List, Dict
+
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy.orm import Session
+
+from . import crud, models, schemas
+from .database import SessionLocal, engine
+
+app = FastAPI()
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World this is my new API!"}
+
+@app.get("/mi_nombre")
+async def root():
+    return {"Hola mi nombre es Laura Aidee Vazquez Dominguez mucho gusto!"}
+
+@app.get("/myname/{name}")
+async def myName(name: str):
+    return {"message": f"Hello {name} this is my new API!"}
